5a9ac33ab9cd1795255e0be83851dfc1878fa8f9
Paul Joy <pjoyjr@gmail.com>
2022-05-10 23:19:38 -0400

    moveable background

--- index.js
+++ index.js
@@ -1,23 +1,64 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
+const map1 = new Image()
+const playerImage = new Image()
 
 canvas.width = 1024
 canvas.height = 576
 
-const map1 = new Image()
-map1.src = './img/map1.png'
-
-const playerImage = new Image()
-playerImage.src = './img/playerDown.png'
-
 c.fillStyle = 'white'
 c.fillRect(0, 0, canvas.width, canvas.height)
 
-map1.onload = () => {
-    c.drawImage(map1, 0, -300)
+map1.src = './img/map1.png'
+playerImage.src = './img/playerDown.png'
+
+class Sprite {
+    constructor({ position, velocity, image }) {
+        this.position = position
+        this.image = image
+    }
+
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
 }
 
-playerImage.onload = () => {
+const background = new Sprite({
+    position: {
+        x: 0,
+        y: -300
+    },
+    image: map1
+})
+
+const player = new Sprite({
+    position: {
+        x: canvas.width / 2 - (playerImage.width / 4) / 2,
+        y: canvas.height / 2 - playerImage.height / 2,
+    },
+    image: playerImage
+})
+
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    background.draw()
+
     c.drawImage(
         playerImage,
         0,
@@ -29,17 +70,49 @@ playerImage.onload = () => {
         playerImage.width / 4,
         playerImage.height
     )
+    if (keys.w.pressed && lastKey === 'w') background.position.y += 3
+    else if (keys.a.pressed && lastKey === 'a') background.position.x += 3
+    else if (keys.s.pressed && lastKey === 's') background.position.y -= 3
+    else if (keys.d.pressed && lastKey === 'd') background.position.x -= 3
 }
+animate()
 
+
+let lastKey = ''
 window.addEventListener('keydown', (e) => {
     switch (e.key) {
         case 'w':
+            keys.w.pressed = true
+            lastKey = 'w'
             break
         case 'a':
+            keys.a.pressed = true
+            lastKey = 'a'
             break
         case 's':
+            keys.s.pressed = true
+            lastKey = 's'
             break
         case 'd':
+            keys.d.pressed = true
+            lastKey = 'd'
+            break
+    }
+})
+
+window.addEventListener('keyup', (e) => {
+    switch (e.key) {
+        case 'w':
+            keys.w.pressed = false
+            break
+        case 'a':
+            keys.a.pressed = false
+            break
+        case 's':
+            keys.s.pressed = false
+            break
+        case 'd':
+            keys.d.pressed = false
             break
     }
 })
