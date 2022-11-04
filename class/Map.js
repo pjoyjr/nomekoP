ebd2f3f3473be4f8223bc4d9e23812e13643f059
Paul Joy <pjoyjr@gmail.com>
2022-11-03 23:54:03 -0400

    pushed final changes

--- class/Map.js
+++ class/Map.js
@@ -344,6 +344,29 @@ class Map {
     }
 
     animate() {
+
+        let gamepads = navigator.getGamepads();
+
+        for (let i = 0; i < gamepads.length; i++) {
+            console.log("Gamepad " + i + ":");
+
+            if (gamepads[i] === null) {
+                console.log("[null]");
+                continue;
+            }
+
+            if (!gamepads[i].connected) {
+                console.log("[disconnected]");
+                continue;
+            }
+
+            console.log("    Index: " + gamepads[i].index);
+            console.log("    ID: " + gamepads[i].id);
+            console.log("    Axes: " + gamepads[i].axes.length);
+            console.log("    Buttons: " + gamepads[i].buttons.length);
+            console.log("    Mapping: " + gamepads[i].mapping);
+        }
+
         this.background.draw()
         this.boundaries.forEach(boundary => {
             boundary.draw()
